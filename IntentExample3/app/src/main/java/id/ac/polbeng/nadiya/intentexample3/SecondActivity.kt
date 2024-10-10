ad8e887842acdc655de0abac3fd0642bc13e7943
nadiyanur <diyay285@gmail.com>
2024-10-10 08:47:24 +0700

    upload File Started

---
+++ IntentExample3/app/src/main/java/id/ac/polbeng/nadiya/intentexample3/SecondActivity.kt
@@ -0,0 +1,33 @@
+package id.ac.polbeng.nadiya.intentexample3
+
+import android.os.Bundle
+import android.content.Intent
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import id.ac.polbeng.nadiya.intentexample3.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bundle = intent.getBundleExtra("main_activity_data")
+        val height = bundle?.getDouble("height")
+        val weight = bundle?.getDouble("weight")
+
+        binding.txtIntentData.text = "Height: $height | Weight: $weight"
+        binding.btnCalculate.setOnClickListener {
+            val intent = Intent()
+            var bmiValue = 0.0
+            if (height != null && weight != null){
+                bmiValue = 703 * (weight / (height * height))
+            }
+            intent.putExtra("second_activity_data", bmiValue)
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+        }
+    }
+}
